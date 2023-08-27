9de56f46682d6ace3705702f78d4fd436147f932
prajwal <pzlmagar@gmail.com>
2023-08-27 14:20:41 +0545

    updated about me section.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
+import AboutMe from "@/components/about-me";
 import Intro from "@/components/intro";
+import SectionDivider from "@/components/section-divider";
 
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
       <Intro/>
+      <SectionDivider/>
+      <AboutMe/>
     </main>
   )
 }
